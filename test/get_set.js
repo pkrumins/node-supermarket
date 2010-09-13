994027bb765b0529b7459b958ed2cb92d7ec84f3
James Halliday <substack@gmail.com>
2010-09-13 01:54:05 -0800

    get/set expresso test

---
+++ test/get_set.js
@@ -0,0 +1,29 @@
+var Store = require('store');
+
+exports['get/set'] = function (assert) {
+    var file = '/tmp/' + Math.floor(Math.random() * 1e8) + '.db';
+    
+    Store(file, function (error, db) {
+        if (error) throw error;
+        
+        db.set('pkrumins', 5, function (sErr) {
+            assert.ok(!sErr);
+            db.get('pkrumins', function (gErr, value) {
+                assert.ok(!gErr);
+                assert.equal(value, 5);
+            });
+        });
+        
+        db.set('substack', 8, function (sErr) {
+            db.get('substack', function (gErr, value) {
+                assert.ok(!gErr);
+                assert.equal(value, 8);
+            });
+        });
+
+        db.get('jesusabdullah', function (err, value) {
+            assert.ok(!err);
+            assert.equal(value, undefined);
+        });
+    });
+};
