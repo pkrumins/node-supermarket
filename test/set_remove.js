868889a60b5370350ddddb75fce60a6eb86b0a06
James Halliday <substack@gmail.com>
2010-09-13 02:51:46 -0800

    remove test, fails

---
+++ test/set_remove.js
@@ -0,0 +1,21 @@
+var Store = require('store');
+
+exports['set/remove'] = function (assert) {
+    var file = '/tmp/' + Math.floor(Math.random() * 1e8) + '.db';
+    
+    Store(file, function (error, db) {
+        if (error) throw error;
+        
+        db.set('o_O', 50, function (sErr) {
+            assert.ok(!sErr);
+            db.remove('o_O', function (rErr, r) {
+                assert.ok(!err);
+                assert.ok(r);
+                db.get('o_O', function (gErr, value) {
+                    assert.ok(!gErr);
+                    assert.equal(arguments.length, 0);
+                });
+            });
+        });
+    });
+};
