0ccc426e99be0eae09361889d83d9ab605120869
Peteris Krumins <peteris.krumins@gmail.com>
2010-09-14 16:48:30 +0300

    test for db.filter(pred,cb,done)

---
+++ test/filter.js
@@ -0,0 +1,42 @@
+var Store = require('supermarket');
+
+exports['filter'] = function (assert) {
+    Store(':memory:', function (err, db) {
+        assert.ok(!err);
+
+        range(0,100).forEach(function (i) {
+            var key = 'key' + (i).toString();
+            db.set(key, i, function (err, k, v) {
+                assert.ok(!err);
+                if (v == 99) {
+                    test_evens();
+                }
+            });
+        });
+
+        function test_evens() {
+            var evens = [];
+            db.filter(
+                function (i) { return i % 2 == 0 },
+                function (err, val) {
+                    assert.ok(!err);
+                    evens.push(val);
+                },
+                function () {
+                    function sorter(a,b) { return a-b }
+                    assert.eql(
+                        evens.sort(sorter),
+                        range(0,100).filter(function (i) { return i % 2 == 0 })
+                    );
+                }
+            );
+        }
+    });
+}
+
+function range(i, j) {
+    var ret = [];
+    for (; i<j; i++) ret.push(i);
+    return ret;
+}
+
